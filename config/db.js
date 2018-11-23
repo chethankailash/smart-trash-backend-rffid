c108e38ae1c1cced9600bcf2855671bcc6891f56
chethankailash <chethankailash3@gmail.com>
2018-11-23 11:46:39 +0530

    modified db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const CONNECTION_URI = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/mcaticketmaster' 
+const CONNECTION_URI = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/mcaticketmasters' 
 mongoose.set('debug',true);
 
 mongoose.connect(CONNECTION_URI, {
