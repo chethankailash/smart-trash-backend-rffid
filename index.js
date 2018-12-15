851fc59f348e67a4d01501f77b6869724fda4a28
chethankailash <chethankailash3@gmail.com>
2018-12-15 21:53:22 +0530

    port changed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 const mongoose = require('./config/db');
 const {Emp} = require('./models/employee.js');
 const bodyParser = require('body-parser');
