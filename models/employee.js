d4cca9543a122cd0df6f3675ef2e1217e4dab8d5
chethankailash <chethankailash3@gmail.com>
2018-12-15 22:39:28 +0530

    model modified

--- models/employee.js
+++ models/employee.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const empSchema = new Schema({
-    ID: {
-        type: String,
+    num: {
+        type: Number,
         required: true
     }
 
